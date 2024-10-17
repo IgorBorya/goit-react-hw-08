7bf90f897cc872790f8b44a13814f709da080d6c
Ihor_Haraniuk <igorgaranyuk133@gmail.com>
2024-10-17 13:10:15 +0300

    refactoring store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,29 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import contactsReducer from "./contactsSlice";
 import filtersReducer from "./filtersSlice";
 import { combineReducers } from "redux";
 
-const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["items"],
-};
-
 const rootReducer = combineReducers({
-  contacts: persistReducer(persistConfig, contactsReducer),
+  contacts: contactsReducer,
   filters: filtersReducer,
 });
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ["persist/PERSIST"],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
